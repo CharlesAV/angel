489f10beb2cf8540c74c8c63ad563f7825eb1c40
Charles Konsor <charles@angelvisiontech.com>
2014-10-06 13:26:33 -0700

    Tweaks to password reminder view.

--- src/views/password/remind.blade.php
+++ src/views/password/remind.blade.php
@@ -3,24 +3,30 @@
 @section('title', 'Forgot Password')
 
 @section('content')
-	<section class="main">
-		<div class="content">
-			<div class="row">
-				<div class="medium-12 small-12 columns">
-					<h1>Forgot Password</h1>
-					Enter the e-mail you used when signing up and we'll send you instructions on how you can' reset your password!<br /><br />
-					
-					{{ Form::open() }}
-						{{ Form::label('email','E-mail') }}</td>
-						{{ Form::text('email', null, array('class'=>'form-control')) }}
-						<input type="submit" value="Submit" class="btn btn-primary button" />
-						{{ Form::hidden('url',Request::url()) }}
-						{{ Form::hidden('redirect',(Input::get('redirect') ? Input::get('redirect') : "/")) }}
-					{{ Form::close() }}
+<div class="content">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Forgot Password</h1>
+			Enter the e-mail you used when signing up and we'll send you instructions on how you can' reset your password!<br /><br />
+			
+			{{ Form::open(array('class' => 'form-horizontal password-remind')) }}
+				<div class="row">
+					<div class="form-group">
+						{{ Form::label('email','E-mail',array('class' => 'col-md-1 control-label')) }}
+						<div class="col-md-3">
+							{{ Form::text('email', null, array('class'=>'form-control')) }}
+						</div>
+						<div class="col-md-3">
+							<input type="submit" value="Submit" class="btn btn-primary button" />
+						</div>
+					</div>
 				</div>
-			</div>
+				{{ Form::hidden('url',Request::url()) }}
+				{{ Form::hidden('redirect',(Input::get('redirect') ? Input::get('redirect') : "/")) }}
+			{{ Form::close() }}
 		</div>
-	</section>
+	</div>
+</div>
 @stop
 
 
